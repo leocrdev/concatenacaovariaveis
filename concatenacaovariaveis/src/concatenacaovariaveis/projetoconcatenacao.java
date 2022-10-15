782efe543a32919bc48dfaff27b2008eae7f371f
LEOCRDEV leocrdev@hotmail.com <LEOCRDEV leocrdev@hotmail.com>
2022-10-15 20:36:31 -0300

    concatenação de variáveis

---
+++ concatenacaovariaveis/src/concatenacaovariaveis/projetoconcatenacao.java
@@ -0,0 +1,20 @@
+package concatenacaovariaveis;
+
+public class projetoconcatenacao {
+
+	
+	/*Main é um auto executável em Java*/
+	public static void main(String[] args) {
+		
+		
+	/*Concatenação = Unir ou juntar dados*/
+	String nome = "Leonardo Ribeiro";
+	String cpf = "000.582.389-89";
+	String telefone = "(62) 96356-0000";
+	String endereco = "Goiânia - GO";
+	
+	System.out.println("Meu nome é: " + nome + ", o CPF é: " + cpf + ", o telefone de contato é: " + telefone + ", a cidade onde eu moro é: " + endereco);
+	
+	
+	}
+}
